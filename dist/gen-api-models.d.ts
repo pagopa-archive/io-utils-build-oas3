c891cf107c446be08ccce66751b62be31adcf69f
Aymen Naghmouchi <aymenadvance@gmail.com>
2019-07-22 10:34:27 +0200

    update.

--- dist/gen-api-models.d.ts
+++ dist/gen-api-models.d.ts
@@ -2,7 +2,7 @@ import { ITuple2 } from "italia-ts-commons/lib/tuples";
 import * as nunjucks from "nunjucks";
 import { OpenAPI, OpenAPIV2 } from "openapi-types";
 export declare function renderDefinitionCode(env: nunjucks.Environment, definitionName: string, definition: OpenAPIV2.DefinitionsObject, strictInterfaces: boolean): Promise<string>;
-export declare function renderOperation(method: string, operationId: string, operation: OpenAPIV2.OperationObject, specParameters: OpenAPIV2.ParametersDefinitionsObject | undefined, securityDefinitions: OpenAPIV2.SecurityDefinitionsObject | undefined, extraHeaders: ReadonlyArray<string>, extraParameters: {
+export declare function renderOperation(method: string, operationId: string, operation: OpenAPIV2.OperationObject, specParameters: OpenAPIV2.ParametersDefinitionsObject | undefined, securityDefinitions: OpenAPIV2.Document["securityDefinitions"], extraHeaders: ReadonlyArray<string>, extraParameters: {
     [key: string]: string;
 }, defaultSuccessType: string, defaultErrorType: string, generateResponseDecoders: boolean): ITuple2<string, ReadonlySet<string>>;
 export declare function isOpenAPIV2(specs: OpenAPI.Document): specs is OpenAPIV2.Document;
